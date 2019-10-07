1deb2a26779e3d7ab71c9f1992233a524d9c35e4
RMaughan93 <rhys.maughan93@gmail.com>
2019-10-07 12:30:47 +0100

    added camera animation that circles the centre of the frame, number need tweaking. Changed initial position on camera on canvas startup

--- src/Animations/shapesAnimation.js
+++ src/Animations/shapesAnimation.js
@@ -5,6 +5,7 @@ const tetra1 = new Tetra(200, 500, 0, 0, scene);
 let sphereScale = 1;
 let cubeScale = 1;
 let tetraScale = 1;
+let timerThing;
 
 function mainLoop(){
   if (analyser) {
@@ -21,7 +22,33 @@ function mainLoop(){
   cube1.shape.rotation.y += 0.01;
   tetra1.shape.rotation.x += 0.01;
   tetra1.shape.rotation.y += 0.01;
-
+  if (timerThing < 1500 ) {
+        camera.position.z -= 3.9;
+        camera.position.x -= 3.9;
+        camera.rotation.y -= 0.003;
+        timerThing +=3;
+        console.log(timerThing);
+      } else if (timerThing >= 1500 && timerThing < 3000 ) {
+        camera.position.z -= 3.9;
+        camera.position.x += 3.9;
+        camera.rotation.y -= 0.003;
+        timerThing+=3;
+        console.log(timerThing);
+      } else if (timerThing >= 3000 && timerThing < 4500 ) {
+        camera.position.z += 3.9;
+        camera.position.x += 3.9;
+        camera.rotation.y -= 0.003;
+        timerThing+=3;
+        console.log(timerThing);
+      } else if (timerThing >= 4500 && timerThing < 6000) {
+        camera.position.z += 3.9;
+        camera.position.x -= 3.9;
+        camera.rotation.y -= 0.003;
+        timerThing+=3;
+        console.log(timerThing);
+      } else {
+        timerThing = 0;
+      }
   renderer.render(scene, camera);
   requestAnimationFrame(mainLoop);
 }
